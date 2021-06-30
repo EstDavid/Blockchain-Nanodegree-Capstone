84ac6d3c6cda5f059001e691017bcbbb5998759e
Vandenynas <stlo.subs@gmail.com>
2021-06-30 08:02:00 +0300

    Wrote test contract and corresponding test file for ZK and ERC721 integration

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,59 +1,82 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+import "./ERC721Mintable.sol";
+import 'openzeppelin-solidity/contracts/drafts/Counters.sol';
+
+contract SolnSquareVerifier is DigitalPropertyToken {
+
+    using Counters for Counters.Counter;
+
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
-
+    /*** Referencing the Verifier contract ***/
+    // Adding state variable referencing Verifier contract
+    Verifier private verifier;
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
-
-
+    constructor (
+                    string memory name,
+                    string memory symbol,
+                    string memory baseTokenURI,
+                    address verifierContract
+                ) 
+                DigitalPropertyToken(name, symbol, baseTokenURI)
+                public {
+                    verifier = Verifier(verifierContract);
+                }
 
 // TODO define a solutions struct that can hold an index & an address
-
+    struct solutions {
+        uint256 index; 
+        address owner;
+        bool isSolution;
+    }
 
 // TODO define an array of the above struct
-
+    //solutions[] solutionsList;
 
 // TODO define a mapping to store unique solutions submitted
-
-
+    mapping(bytes32 => solutions) public uniqueSolutions;
 
 // TODO Create an event to emit when a solution is added
-
-
+    event SolutionAdded(bytes32 key, address owner);
 
 // TODO Create a function to add the solutions to the array and emit the event
-
-
+    function addSolution(bytes32 key, uint256 tokenId, address solutionOwner) public {
+        uniqueSolutions[key] = solutions (tokenId, solutionOwner, true);
+        emit SolutionAdded(key, solutionOwner);
+    }
 
 // TODO Create a function to mint new NFT only after the solution has been verified
 //  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
+//  - make sure you handle metadata as well as tokenSupply
+    function mintNFT(
+                    uint[2] memory a,
+                    uint[2][2] memory b,
+                    uint[2] memory c,
+                    uint[2] memory inputs,
+                    address solutionOwner
+                    ) 
+                    public {
+        bool verified = verifier.verifyTx(a, b, c, inputs);
+        require(verified, "Solution is not valid");
+        bytes32 key = keccak256(abi.encodePacked(a, b, c, inputs, solutionOwner));
+        require(uniqueSolutions[key].isSolution == false, "Solution already exists");
+        uint256 newTokenId = totalSupply() + 1;
+        mint(solutionOwner, newTokenId);
+        addSolution(key, newTokenId, solutionOwner);        
+    }
 
-  
+}
 
+contract Verifier {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function verifyTx(
+        uint[2] calldata a,
+        uint[2][2] calldata b,
+        uint[2] calldata c,
+        uint[2] calldata input
+    ) 
+    external
+    view
+    returns(bool);
+}
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,97 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+var verifierContract = artifacts.require("Verifier");
+var solnSquareVerifierContract = artifacts.require("SolnSquareVerifier");
+var SampleProof = require('../../zokrates/code/square/proof');
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+contract('TestSolnSquareVerifier', accounts => {
+    const account_one = accounts[0];
+    const account_two = accounts[1];
+    const a = SampleProof.proof.a;
+    const b = SampleProof.proof.b;
+    const c = SampleProof.proof.c;
+    const inputs = SampleProof.inputs;
+
+    const tokenName = "Digital Property Token";
+    const symbol = "DPT";
+    const baseTokenURI = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
+    
+    beforeEach(async function () {
+        this.verifierContract = await verifierContract.new(account_one);
+        this.solnSquareVerifierContract = 
+        await solnSquareVerifierContract.new(
+                                                tokenName,
+                                                symbol,
+                                                baseTokenURI,
+                                                this.verifierContract.address,
+                                                {from: account_one}
+                                                );
+    });
+
+    // Test if a new solution can be added for contract - SolnSquareVerifier
+    it('should add a new solution', async function () {
+        let tokenId = 1;
+
+        let expectedEvent = "SolutionAdded";
+
+        let key = web3.utils.soliditySha3(
+                                            a[0],
+                                            a[1],
+                                            b[0][0],
+                                            b[0][1],
+                                            b[1][0],
+                                            b[1][1],
+                                            c[0],
+                                            c[1],
+                                            inputs[0],
+                                            inputs[1],
+                                            account_two);
+
+        let eventTx = await this.solnSquareVerifierContract.addSolution(key, tokenId, account_two, {from: account_two});
+        let result = await this.solnSquareVerifierContract.uniqueSolutions.call(key);
+
+        let eventType = eventTx.logs[0].event;
+        let eventKey = eventTx.logs[0].args.key;
+        let eventOwner = eventTx.logs[0].args.owner;
+        let tokenOwner = result.owner;
+        let solutionCreated = result.isSolution;
+
+        assert.equal(eventType, expectedEvent, "A 'SolutionAdded' event should have been emitted");
+        assert.equal(eventKey, key, "Key of the solution emitted in the event should be correct");
+        assert.equal(eventOwner, account_two, "Account owner of the solution emitted in the event should be correct");
+        assert.equal(tokenOwner, account_two, "Account owner of the solution stored in the contract should be correct");
+        assert.equal(solutionCreated, true, "The 'isSolution' variable of the solution should be set to true");
+    });
+
+    // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+    it('should mint a new token', async function () {
+
+        let expectedEvent = "SolutionAdded";
+
+        let key = web3.utils.soliditySha3(
+                                            a[0],
+                                            a[1],
+                                            b[0][0],
+                                            b[0][1],
+                                            b[1][0],
+                                            b[1][1],
+                                            c[0],
+                                            c[1],
+                                            inputs[0],
+                                            inputs[1],
+                                            account_two);
+
+        let eventTx = await this.solnSquareVerifierContract.mintNFT(a, b, c, inputs, account_two, {from: account_one});
+        let result = await this.solnSquareVerifierContract.uniqueSolutions.call(key);
+
+        let eventType = eventTx.logs[1].event;
+        let eventKey = eventTx.logs[1].args.key;
+        let eventOwner = eventTx.logs[1].args.owner;
+        let tokenOwner = result.owner;
+        let solutionCreated = result.isSolution;
+
+        assert.equal(eventType, expectedEvent, "A 'SolutionAdded' event should have been emitted");
+        assert.equal(eventKey, key, "Key of the solution emitted in the event should be correct");
+        assert.equal(eventOwner, account_two, "Account owner of the solution emitted in the event should be correct");
+        assert.equal(tokenOwner, account_two, "Account owner of the solution stored in the contract should be correct");
+        assert.equal(solutionCreated, true, "The 'isSolution' variable of the solution should be set to true");
+    });
+})
